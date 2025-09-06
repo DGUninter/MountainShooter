a2d581cf81ec485fe15ab971727c94cf81f9bd28
Davi <davi@davi.com>
2025-09-06 17:33:07 -0300

    Add Buttons

--- Code/Const.py
+++ Code/Const.py
@@ -1,2 +1,13 @@
+# C
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_BLACK = (0, 0, 0)
+#M2
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCORE',
+               'EXIT')
+# M
 MIN_WIDTH = 576
 MIN_HEIGHT = 324
--- Code/Game.py
+++ Code/Game.py
@@ -11,16 +11,7 @@ class Game:
         self.window = pygame.display.set_mode(size=(MIN_WIDTH, MIN_HEIGHT))
 
     def run(self):
-        pygame.mixer_music.load('./asset/Menu.mp3')
-        pygame.mixer_music.play(-1)
-
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-            # # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #        print('Quiting')
-            #        pygame.quit()  # Close window
-            #        quit()  # end pygame
